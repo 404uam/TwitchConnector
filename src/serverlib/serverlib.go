c9070134bdf1880426abf9d451b82603c494282e
Louis Mau <louis.mau@alumni.ubc.ca>
2019-02-16 23:55:16 -0800

    Added Human readable error functions from Twitch code samples

--- src/serverlib/serverlib.go
+++ src/serverlib/serverlib.go
@@ -5,6 +5,9 @@ import (
 	"os"
 )
 
+//TODO Get streamer tokens && Store token in a safe place
+//TODO Refresh streamer token periodically
+
 type ServerCred struct {
 	client_id     string
 	client_secret string
@@ -32,4 +35,36 @@ func IsErr(msg string, e error) {
 	if e != nil {
 		ErrLog.Fatalf("%s, err = %s\n", msg, e.Error())
 	}
+}
+
+/********* Added from twitch oauth authorization code sample *****/
+/* Can be accessed here https://github.com/twitchdev/authentication-samples */
+
+// HumanReadableError represents error information
+// that can be fed back to a human user.
+//
+// This prevents internal state that might be sensitive
+// being leaked to the outside world.
+type HumanReadableError interface {
+	HumanError() string
+	HTTPCode() int
+}
+
+// HumanReadableWrapper implements HumanReadableError
+type HumanReadableWrapper struct {
+	ToHuman string
+	Code    int
+	error
+}
+
+func (h HumanReadableWrapper) HumanError() string { return h.ToHuman }
+func (h HumanReadableWrapper) HTTPCode() int      { return h.Code }
+
+// AnnotateError wraps an error with a message that is intended for a human end-user to read,
+// plus an associated HTTP error code.
+func AnnotateError(err error, annotation string, code int) error {
+	if err == nil {
+		return nil
+	}
+	return HumanReadableWrapper{ToHuman: annotation, error: err}
 }
